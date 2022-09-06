6c1e4fe7d7667dcfba6c772d2524a6e4e1df42df
Joseph Driscoll <driscoll.recording@gmail.com>
2022-09-06 15:50:52 -0400

    initializing ASP.NET App

---
+++ MVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCApp.Models;
+
+namespace MVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            DogViewModel dog = new DogViewModel() 
+            { 
+                Name = "Fippy", 
+                Age = 2 
+            };
+            return View(dog);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
